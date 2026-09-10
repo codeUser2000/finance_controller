d63dbb920d79a9656a38779c13ce058802969733
codeUser2000 <aidahakobyanf12@gmail.com>
2026-09-10 23:04:37 +0400

    Update Vite configuration to change API proxy target from port 4003 to 4004 and remove allowed hosts setting for the development server.

--- front/vite.config.js
+++ front/vite.config.js
@@ -4,12 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: [
-      'unbroken-pawing-culprit.ngrok-free.dev',
-    ],
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:4003',
+        target: 'http://127.0.0.1:4004',
         changeOrigin: true, 
       },
     },
